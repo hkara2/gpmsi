ff9622d54e4849dfdceb97632e98f39b02c5f455
hkaradimas <hkaradimas@ch-sudessonne.fr>
2023-08-17 18:03:13 +0200

    modifications pour ne plus avoir l'avertissement de compilation

--- src/test/java/fr/karadimas/groovytests/GroovyLibsTestLaunch.java
+++ src/test/java/fr/karadimas/groovytests/GroovyLibsTestLaunch.java
@@ -10,7 +10,7 @@ public class GroovyLibsTestLaunch {
   public static final void run() {
     //invocation dynamique car Gradle ne "voit" pas LibsTest depuis java pour l'instant, il faut que je trouve le réglage à faire.
     try {
-      Class clazz = GroovyLibsTestLaunch.class.forName("fr.karadimas.groovytests.LibsTest");
+      Class<?> clazz = Class.forName("fr.karadimas.groovytests.LibsTest");
       if (clazz == null) throw new ClassNotFoundException("Impossible de charger fr.karadimas.groovytests.LibsTest");
       Method m = (Method) clazz.getMethod("runAllTests");
       if (m == null) throw new NoSuchMethodError("Ne trouve pas la methode runAllTests");
