a03e725f6d870c6ae47cb03727179638733e3fbe
hkaradimas <hkaradimas@ch-sudessonne.fr>
2023-09-25 19:48:55 +0200

    adaptations

--- script-templates/a-copier-coller/RSA/rsa-chir-digestive-onco.bat
+++ script-templates/a-copier-coller/RSA/rsa-chir-digestive-onco.bat
@@ -5,17 +5,20 @@ rem puis a adapter
 
 setlocal EnableExtensions
 
-rem Adapter GPMSI_HOME dans la ligne ci-dessous si gpmsi est installe ailleurs
-set GPMSI_HOME=C:\app\gpmsi\v@PROJECT_VERSION@
+rem Met la valeur par defaut pour GPMSI_HOME si non defini
+if not defined GPMSI_HOME set GPMSI_HOME=C:\app\gpmsi\v@PROJECT_VERSION@
 
 set APP=%GPMSI_HOME%\gpmsi.bat
 
 set SCRIPT=%GPMSI_HOME%\scripts\groovy\regles\rsa_chir_digestive_onco.groovy
 
 rem Fichier RSA
-set RSA=<mettre_ici_chemin_du_fichier_csv_en_entree>
+set RSA=mettre_ici_chemin_du_fichier_csv_en_entree
 
-rem Fichier de sortie avec les NRSA (par ex. NRSAs_chir_dig_onco.csv), ce sera 'b'
-set B=<mettre_ici_chemin_du_fichier_csv_en_sortie>
+rem Fichier TRA (format csv de DRUIDES)
+set TRA=mettre_ici_chemin_du_fichier_tra_en_entree
 
-"%APP%" -script "%SCRIPT%" -a:input "%RSA%" -a:output "%B%"
+rem Fichier de sortie avec les NRSA (par ex. NRSAs_chir_dig_onco.csv), ce sera 'B'
+set B=mettre_ici_chemin_du_fichier_csv_en_sortie
+
+"%APP%" -script "%SCRIPT%" -a:input "%RSA%" -a:tra "%TRA%" -a:output "%B%"
