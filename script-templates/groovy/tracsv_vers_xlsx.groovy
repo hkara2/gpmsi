dede5762ffb62f8801e5ddf41e40a91798447144
hkaradimas <hkaradimas@ch-sudessonne.fr>
2023-09-25 19:45:30 +0200

    creation

---
+++ script-templates/groovy/tracsv_vers_xlsx.groovy
@@ -0,0 +1,52 @@
+/**☺:encoding=UTF-8:
+ * Lire un fichier TRA au format CSV (Format de DRUIDES à partir de avril 2023)
+ * et le convertir au format .xlsx, avec les noms de colonne suivants :
+ * nligne;nrss;nadl;ddsej;dfsej;ghm;hash_tra
+ *
+ * Arguments :
+ * -a:input chemin_du_fichier_entree
+ * -a:output chemin_du_fichier_sortie
+ * Exemples :
+ * C:\app\gpmsi\v@PROJECT_VERSION@\gpmsi -script C:\app\gpmsi\v@PROJECT_VERSION@\scripts\groovy\tracsv_vers_xlsx.groovy -a:input montra.txt -a:output montra.xlsx
+ */
+import fr.karadimas.gpmsi.StringTable
+import fr.karadimas.gpmsi.poi.XlsxHelper
+import static fr.karadimas.gpmsi.StringUtils.isTrimEmpty
+
+tras = new StringTable("TRA")
+//lire le TRA dans la StringTable
+tras.readFrom(new File(args.input), ["nligne", "nrss", "nadl", "ddsej", "dfsej", "ghm", "hash_tra"] as String[], "ISO-8859-1", ';' as char)
+
+//transformer les dates en utilisant les deux formats qui sont présents par défaut
+//tras.transformDates("ddsej", pmsiDateFormat, frenchDateFormat)
+//tras.transformDates("dfsej", pmsiDateFormat, frenchDateFormat)
+
+
+//for (int i = 0; i < tras.rowCount; i++) {
+//    cur_row = tras[i]
+//    for (int j = 0; j < cur_row.size(); j++) {
+//        val = cur_row[j]
+//        println "$i,$j:$val"
+//    }
+//}
+
+xlsx = new XlsxHelper("tra")
+
+tras.columnNames.each {name-> xlsx.addCell(name)}
+xlsx.newRow()
+cal = Calendar.getInstance()
+tras.each {row->
+    row.eachWithIndex {colval, ix->
+        if (!isTrimEmpty(colval) && (ix == 3 || ix == 4)) {
+            cal.setTime(pmsiDateFormat.parse(colval))
+            xlsx.addCell(cal)
+        }
+        else {
+          xlsx.addCell(colval)
+        }
+    }
+    xlsx.newRow()
+}
+
+xlsx.setOutput(new File(args.output))
+xlsx.writeFileAndClose()
---
+++ script-templates/menu.bat
@@ -0,0 +1,11 @@
+rem Lancer le fichier menu.html qui est un menu pour acceder aux commandes
+rem et aux repertoires
+
+setlocal EnableExtensions
+
+if not defined GPMSI_HOME set GPMSI_HOME=C:\app\gpmsi\v@PROJECT_VERSION@
+
+set APP=%GPMSI_HOME%\gpmsi.bat
+set SCRIPT=%GPMSI_HOME%\scripts\groovy\fr\karadimas\gpmsi\local\start_menu.groovy
+
+call %APP% -script %SCRIPT% -a:input menu.html
