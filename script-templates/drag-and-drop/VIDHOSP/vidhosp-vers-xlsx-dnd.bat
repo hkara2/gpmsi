69a30169065b1c8354d03229a5d7d9142ac16090
hkaradimas <hkaradimas@ch-sudessonne.fr>
2023-12-27 16:13:28 +0100

    reparation accents

--- script-templates/drag-and-drop/VIDHOSP/vidhosp-vers-xlsx-dnd.bat
+++ script-templates/drag-and-drop/VIDHOSP/vidhosp-vers-xlsx-dnd.bat
@@ -1,7 +1,7 @@
 rem Conversion de fichier VIDHOSP en ficher CSV
 rem Exemple d'utilisation :
 rem vidhosp-vers-csv.bat "C:\Local\GROUPAGE\2019\M12\200117-pmsipilot\VH200117\VIDHOSP_MCO.txt"
-rem N.B. marche si on fait un "glisser-déposer" sur le fichier bat
+rem N.B. marche si on fait un "glisser-deposer" sur le fichier bat
 
 setlocal enableextensions
 
@@ -9,7 +9,7 @@ if not defined GPMSI_HOME set GPMSI_HOME=C:\app\gpmsi\v@PROJECT_VERSION@
 
 set APP=%GPMSI_HOME%\gpmsi.bat
 
-rem Chemin du script. Mettre repertoire de dev C:\hkchse\dev\pmsixml ou bien C:\app\pmsixml\1.8
+rem Chemin du script.
 set SCRIPT=%GPMSI_HOME%\scripts\groovy\mono_vers_xlsx.groovy
 
 rem Fichier VIDHOSP a transformer
