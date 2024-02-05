8f5ff74a6a6c54f04919b7303dcc1877493d4739
hkaradimas <hkaradimas@ch-sudessonne.fr>
2024-02-05 19:07:46 +0100

    essais supplementaires

--- src/main/groovy/fr/karadimas/gpmsi/pmsi_rules/ccam/CcamCodePresence.groovy
+++ src/main/groovy/fr/karadimas/gpmsi/pmsi_rules/ccam/CcamCodePresence.groovy
@@ -1,3 +1,8 @@
+/**:encoding=UTF-8:
+ * Attention encore en cours de mise au point !
+ * Il y a certainement des bugs, et l'API va encore beaucoup changer !
+ *
+ */
 package fr.karadimas.gpmsi.pmsi_rules.ccam;
 
 import java.util.ArrayList;
@@ -14,7 +19,7 @@ import fr.karadimas.pmsixml.FszGroup;
 
 /**
  * Selection selon la présence de critères sur les codes CCAM.
- * Pour l'instant on fait une recherche simple sur une liste, qui contient des codes CCAM et/ou 
+ * Pour l'instant on fait une recherche simple sur une liste, qui contient des codes CCAM et/ou
  * des expressions régulieres.
  * L'extension PMSI est un problème ; dans certains outils (DxCare ...) l'acte lorsqu'il
  * n'a pas d'extension PMSI est envoyé avec une extension PMSI '-00'.
@@ -25,9 +30,9 @@ import fr.karadimas.pmsixml.FszGroup;
  * dans le RUM, le -00 est enlevé.
  * Pour le RSA il n'y a pas de problème, CCAM à 7 caractères et extension PMSI sur 2 caractères
  * (donc sans le '-') sont deux champs séparés.
- * 
- * Donc pour les expressions régulières. 
- * 
+ *
+ * Donc pour les expressions régulières.
+ *
  * Il y a une syntaxe simplifiée (non encore implémentée) :<br>
  * <ul>
  * <li><code>/a:&lt;expression activité&gt;</code>
@@ -42,35 +47,35 @@ import fr.karadimas.pmsixml.FszGroup;
  * L'expression est :
  * <expression valeur>[, <expression valeur>]
  * L'expression valeur est :
- * valeur [& valeur]
- * Ainsi si on veut rechercher l'acte 
+ * valeur [&amp; valeur]
+ * Ainsi si on veut rechercher l'acte
  * HHFA016 Appendicectomie, par coelioscopie ou par laparotomie avec préparation par coelioscopie
  * avec à la fois le modificateur P et le modificateur U, on écrit :
- * HHFA016/m:P&U
+ * HHFA016/m:P&amp;U
  *
  * Pour les modificateurs, l'expression concerne la zone d'acte courante.
- * 
+ *
  * Pour les autres critères, ils concernent le RUM courant, et les actes qui ont le même délai
- * à partir de l'entrée du séjour (lorsque la condition est '&').
- * 
+ * à partir de l'entrée du séjour (lorsque la condition est '&amp;').
+ *
  * Exemple : on veut vérifier qu'il y a bien dans le RUM un acte JAFA002 qui est une fois avec une
  * activité 1 et une autre fois avec une activité 4, avec le même délai à partir de l'entrée du séjour :
  * <br>
- * 
+ *
  *  <code>JAFA002/a:1&4</code>
- *  
+ *
  *  <br>
- *  
- * 
+ *
+ *
  * @author hkaradimas
  */
 public class CcamCodePresence
-    implements PmsiCriterion 
+    implements PmsiCriterion
 {
   final static Logger lg = LogManager.getLogger(CcamCodePresence.class)
- 
+
   public static Pattern ccamPattern =  Pattern.compile(/[A-Z][A-Z][A-Z][A-Z]\d\d\d(-\d\d)?/) //pattern pour un code CCAM (avec extention PMSI) autorisé
-  
+
   /**
    * Classe interne pour stocker les critères de sélection d'un code avec les expressions qui vont avec
    */
@@ -80,27 +85,27 @@ public class CcamCodePresence
     AoeNode exprActivites
     AoeNode exprModificateurs
   }
-  
+
   List<String> includedCodes = new ArrayList<>();
   List<Pattern> includedPatterns = new ArrayList<>();
-  
+
   //sélectionner si au moins une activité CCAM est présente dans cette liste
   List<String> includedActivities = new ArrayList<>();
   //exclure si au moins une activité CCAM est présente dans cette liste (a priorité sur includedActivities)
   List<String> excludedActivities = new ArrayList<>();
-  
+
   //sélectionner si au moins un modificateur est présent dans cette liste
   List<String> includedModifiers = new ArrayList<>();
   //exclure si au moins un modificateur est présent dans cette liste (a priorité sur includedModifiers)
   List<String> excludedModifiers = new ArrayList<>();
 
-  
+
   /**
    * Constructeur simple avec juste un ensemble de codes à rechercher.
    * Les codes sont soit des codes CCAM normaux si ils sont acceptés par le #ccamPattern,
    * soit une expression régulière sinon.
-   * Si le code est un code à 7 caractères, il lui est ajouté '-00' car c'est ce que
-   * l'on retrouve dans les zones d'activités des RSS et RSA.
+   * Si le code est un code à 7 caractères, il sera comparé tel quel car les
+   * codes lus dans les RSS et RSA ont les espaces de fin enlevés avant comparaison.
    * Donc si par exemple on veut tous les codes qui commencent par JGNE171, on a 2 solutions :
    * <ul>
    * <li>['JGNE171.*']
@@ -117,7 +122,7 @@ public class CcamCodePresence
       else includedPatterns.add(Pattern.compile(code))
     }
   }
-  
+
   /**
    * Constructeur qui prend une liste.
    * Appelle ensuite le constructeur qui prend un ensemble (Set) en transformant la "List" en "Set".
@@ -126,7 +131,7 @@ public class CcamCodePresence
   public CcamCodePresence(List<String> codesToInclude) {
     this(codesToInclude as Set)
   }
-  
+
   /*
    * Date de réalisation
    * Code CCAM
@@ -143,10 +148,10 @@ public class CcamCodePresence
    */
   @Override
   public boolean eval(HashMap context) {
-    
+
     FszGroup rum = context['rum']
     FszGroup rsa = context['rsa']
-    
+
     Set ccamCodes = [] as Set
 
     if (rsa != null) {
@@ -168,7 +173,7 @@ public class CcamCodePresence
     }
     else if (rum != null) {
       //println "rum:$rum"
-      rum.ZA.txtCCCA.each { code -> 
+      rum.ZA.txtCCCA.each { code ->
         if (code.endsWith('-00')) code = code[0..-4] //enlever les 3 derniers caractères -00 qui ne sont pas pertinents
         lg.debug("ccam '$code'")
         ccamCodes.add(code)
@@ -191,3 +196,36 @@ public class CcamCodePresence
   }
 
 }
+
+
+/*
+ * N.B. voici la definition (avec les noms pmsixml) de la zone actes dans un RSS :
+ * ZA;Date de réalisation                                      ;DR   ;8;*;*;F;N;N;NA/NA        ;JJMMAAAA Signalement si non renseigné                    ;;
+ * ZA;Code CCAM                                                ;CCCA ;7;*;*;O;A;A;NA/NA        ;                                                         ;;
+ * ZA;Extension PMSI                                           ;XPMSI;3;*;*;F;A;A;NA/NA        ;ATTENTION, l’extension est obligatoire lorsqu’elle Existe;;
+ * ZA;Phase                                                    ;PHAS ;1;*;*;O;A;A;NA/NA        ;                                                         ;;
+ * ZA;Activité                                                 ;ACTI ;1;*;*;O;A;A;NA/NA        ;                                                         ;;
+ * ZA;Extension documentaire                                   ;XDOCU;1;*;*;F;A;A;NA/NA        ;                                                         ;;
+ * ZA;Modificateurs                                            ;MODIF;4;*;*;F;A;A;Gauche/Espace;                                                         ;;
+ * ZA;Remboursement exceptionnel                               ;REXC ;1;*;*;F;A;A;NA/NA        ;                                                         ;;
+ * ZA;Association non prévue                                   ;ASSNP;1;*;*;F;A;A;NA/NA        ;                                                         ;;
+ * ZA;Nombre de réalisations de l'acte n° nZA pendant le séjour;NREAL;2;*;*;O;A;A;Droite/Zéro  ;                                                         ;;
+ *
+ * L'extension PMSI est obligatoire lorsqu'elle est présente.
+ * De ce fait, si on cherche un code qui n'a pas d'extension tel que DEQP003
+ * ça va fonctionner sur une recherche CCCA+XPMSI à 'DEQP003   '.
+ * Par contre
+ * Exemple : le code
+ * FCFC001    (Curage lymphonodal [ganglionnaire] iliaque, par cœlioscopie ou par rétropéritonéoscopie)
+ * évolue en deux codes avec extension :
+ * FCFC001-30 (Curage lymphonodal [ganglionnaire] iliaque, par cœlioscopie ou par rétropéritonéoscopie, sans assistance par robot)
+ * FCFC001-40 (Curage lymphonodal [ganglionnaire] iliaque, par cœlioscopie ou par rétropéritonéoscopie, avec assistance par robot)
+ * De ce fait si on recherche tous les codes de curage lymphonodal, avant il
+ * fallait faire une recherche sur FCFC001, après la mise en oeuvre de la nouvelle
+ * nomenclature il faut rechercher FCFC001-30 et FCFC001-40.
+ * L'utilisation d'une recherche par expression régulières simplifie cela, il
+ * suffit de chercher FCFC001.*
+ *
+
+
+ */
