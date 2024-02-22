20179739c64c4c7bd57660b6601831dbcc772b68
hkaradimas <hkaradimas@ch-sudessonne.fr>
2024-02-22 19:01:11 +0100

    corrections cosmetiques

--- script-templates/gui/RPU/rpu-vers-csv-gui.bat
+++ script-templates/gui/RPU/rpu-vers-csv-gui.bat
@@ -4,7 +4,7 @@ setlocal EnableExtensions
 
 if not defined GPMSI_HOME set GPMSI_HOME=C:\app\gpmsi\v@PROJECT_VERSION@
 
-set APP=%GPMSI_HOME%\scripts\gpmsi.bat
+set APP=%GPMSI_HOME%\gpmsi.bat
 set SCRIPT=%GPMSI_HOME%\scripts\groovy\rpu_vers_csv_gui.groovy
 
 %APP% -script %SCRIPT%
