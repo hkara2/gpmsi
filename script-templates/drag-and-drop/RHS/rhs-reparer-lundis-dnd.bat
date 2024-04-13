1684fa1064f684906a649d2ce32310bde74372e4
hkaradimas <hkaradimas@ch-sudessonne.fr>
2024-04-13 19:22:24 +0200

    ajout d'un paramètre interactif pour demander période de fin

--- script-templates/drag-and-drop/RHS/rhs-reparer-lundis-dnd.bat
+++ script-templates/drag-and-drop/RHS/rhs-reparer-lundis-dnd.bat
@@ -4,6 +4,7 @@ rem Exemple d'utilisation :
 rem rhs-reparer-lundis-dnd.bat SSR_RHSN_DateRHS_20220412135017.txt
 rem N.B. marche si on fait un "glisser-deposer" sur le fichier bat (attention echoue si le nom contient une virgule !)
 rem Attention pour l'instant ne marche pas sur les periodes a cheval
+rem Attention demande interactivement la periode de fin !
 
 setlocal enableextensions
 
@@ -14,5 +15,5 @@ set SCRIPT=%GPMSI_HOME%\scripts\groovy\rhs_reparer_lundis.groovy
 
 rem Fichier RHS a reparer (le ~ enleve les guillemets)
 set A=%~1
-call "%APP%" -script "%SCRIPT%" -a:input "%A%"
+call "%APP%" -script "%SCRIPT%" -a:input "%A%" -a:periode "?sEntrez le mois de fin de la periode (AAAAMM)"
 pause
