c3ccde2f2a773efa3333d7a9e21fae4307ade2f8
hkaradimas <hkaradimas@ch-sudessonne.fr>
2024-04-13 19:28:45 +0200

    ajout d'un drapeau pour avoir en sortie les colonnes DENT et DSOR

--- script-templates/groovy/vidhosp_sejours_a_cheval.groovy
+++ script-templates/groovy/vidhosp_sejours_a_cheval.groovy
@@ -11,6 +11,9 @@
   -a:input vh.txt ^
   -a:output sej-a-cheval.csv
  *
+ * Drapeau supplementaire pour ajouter les colonnes DENT et DSOR pour dates entree et sortie de l'hôpital :
+ * -f:avecdates
+ *
  * (C) Harry Karadimas 2024, CHSE
  */
 import fr.karadimas.gpmsi.CsvDestination
@@ -29,8 +32,13 @@ vidhosp {
   headerEmitted = false
 
   onInit {
+      avecdates = flags.contains('avecdates')
       outf = new CsvDestination(new File(outputFilePath), "cp1252")
       outf.f 'NADL' //envoyer l'en-tête
+      if (avecdates) {
+        outf.f 'DENT'
+        outf.f 'DSOR'
+      }
       outf.endRow()
   }
 
@@ -41,8 +49,10 @@ vidhosp {
     if (anneeEntree == anneeSortie) return //sortir si entrée et sortie sont la même année
 
     outf.f vh.txtNADL
-    //outf.f vh.DENT
-    //outf.f vh.DSOR
+    if (avecdates) {
+      outf.f formatAsFrenchDate(vh.DENT.toDate())
+      outf.f formatAsFrenchDate(vh.DSOR.toDate())
+    }
     outf.endRow()
   }
 
