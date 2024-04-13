cb7bccd5a6facc4160cc038c94e583c3c4945e32
hkaradimas <hkaradimas@ch-sudessonne.fr>
2024-04-13 19:27:46 +0200

    quelques explications en plus

--- src/main/groovy/fr/karadimas/gpmsi/pmsi_rules/PmsiRuleEngine.groovy
+++ src/main/groovy/fr/karadimas/gpmsi/pmsi_rules/PmsiRuleEngine.groovy
@@ -3,33 +3,47 @@ package fr.karadimas.gpmsi.pmsi_rules
 import fr.karadimas.pmsixml.FszGroup
 
 /**
- * Moteur de regles tres simpliste que l'on peut appeler pour evaluation de
- * chaque RUM par exemple
+ * Moteur de regles tres simpliste pour l'instant que l'on peut appeler pour evaluation de
+ * chaque RUM par exemple.
+ *
+ * Idees futures pour augmenter les possibilités :
+ * - Ajouter une API pour avoir les autres RUMs d'un même RSS, ainsi que le parcours (UMs dans l'ordre chronologique)
+ * - Ajouter une API pour avoir les autres RSS pour le même patient, avec requête sur l'ordre (suivant/précédent), et sur le temps (de l'année dernière, par ex.)
+ * - Ajouter une API pour avoir les autres RSA pour le même patient, avec requête sur l'ordre (suivant/précédent), et sur le temps (de l'année dernière, par ex.)
+ * - API pour collecter diagnostics, actes, sur un ensemble de RUM/RSS/RSA
+ * L'idée étant que l'on donne un ensemble de RSS+VIDHOSP, ou bien un ensemble de RSA+TRA+VIDHOSP, et qu'ensuite
+ * on parcourt les données à analyser, mais cette fois avec un contexte riche qui permet d'analyser l'antériorité
+ * voire quelque chose qui s'est produit plus tard (par ex. le patient recoit des
+ * chimios mais à aucun moment on n'a de DP de cancer).
+ * A voir aussi : stockage de valeurs qui contrôlent telle ou telle évaluation
+ * pour enlever les faux déclenchements (ex : on n'a pas trouvé de DP de cancer
+ * parce que en fait le cancer a été traité à l'extérieur, on ne veut donc pas
+ * qu'il y ait un avertissement à chaque fois)
  */
 class PmsiRuleEngine {
-    
+
     List<PmsiRule> rules = []
     def context = [:]
-    
+
     PmsiRuleEngine() {
         context['engine'] = this
         context['out'] = new PrintWriter(System.out, true)
         context['collect'] = []
     }
-    
+
     void add(PmsiRule rule) { rules << rule }
-    
+
     /** initialiser toutes les regles */
     void initialize() {
         rules.each() { rule ->
             init(context)
         }
     }
-    
+
     /**
      * a appeler si on evalue un RUM
      * @param g Le RUM/RSS a evaluer
-     * @return Le nombre de regles qui ont ete evaluees a true 
+     * @return Le nombre de regles qui ont ete evaluees a true
      */
     int evalRum(FszGroup g) {
         int ntrue = 0
@@ -42,11 +56,11 @@ class PmsiRuleEngine {
         }
         return ntrue;
     }
-    
+
     /**
      * a appeler si on evalue un RSA
      * @param rsa Le RSA
-     * @return Le nombre de regles qui ont ete evaluees a true 
+     * @return Le nombre de regles qui ont ete evaluees a true
      */
     int evalRsa(FszGroup rsa) {
         int ntrue = 0
@@ -59,9 +73,9 @@ class PmsiRuleEngine {
         }
         return ntrue;
     }
-    
+
     Map getContext() { context }
-    
+
 }
 
 
