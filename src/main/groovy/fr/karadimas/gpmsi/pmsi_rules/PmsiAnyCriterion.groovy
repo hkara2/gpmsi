3fd6c9f19b06238270d3286e0299b24ee0efae7a
hkaradimas <hkaradimas@ch-sudessonne.fr>
2024-09-04 19:40:15 +0200

    meilleure prise en charge de OR

--- src/main/groovy/fr/karadimas/gpmsi/pmsi_rules/PmsiAnyCriterion.groovy
+++ src/main/groovy/fr/karadimas/gpmsi/pmsi_rules/PmsiAnyCriterion.groovy
@@ -12,6 +12,9 @@ class PmsiAnyCriterion
     extends ArrayList<PmsiCriterion> 
     implements PmsiCriterion 
 {
+    public PmsiAnyCriterion(PmsiCriterion ... criteria) {
+        addAll(criteria)
+    }
 
     boolean eval(HashMap context) {
         if (size() == 0) return false
---
+++ src/main/groovy/fr/karadimas/gpmsi/pmsi_rules/PmsiOr.groovy
@@ -0,0 +1,11 @@
+package fr.karadimas.gpmsi.pmsi_rules
+import groovy.transform.InheritConstructors
+
+/**
+ * Identique à PmsiAnyCriterion.
+ */
+@InheritConstructors
+class PmsiOr
+    extends PmsiAnyCriterion
+{
+}
