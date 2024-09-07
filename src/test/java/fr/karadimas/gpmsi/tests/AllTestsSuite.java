014eb55485741c38b75606c54d7676fc993aac8e
hkaradimas <hkaradimas@ch-sudessonne.fr>
2024-09-07 10:24:36 +0200

    désactivation des tests qui faisaient doublon avec les @Test

--- src/test/java/fr/karadimas/gpmsi/tests/AllTestsSuite.java
+++ src/test/java/fr/karadimas/gpmsi/tests/AllTestsSuite.java
@@ -3,16 +3,20 @@ package fr.karadimas.gpmsi.tests;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
-@RunWith(Suite.class)
+//mis en commentaire : éxécutait tout en double ; Gradle détecte et exécute déjà ce qui est marqué par @Test
 
-@Suite.SuiteClasses({
+//@RunWith(Suite.class)
+
+
+/* @Suite.SuiteClasses({
     ExcelFileCreationTest.class,
-    H2Tests.class,
-    PhonexTest.class,
+    //H2Tests.class, //pas la peine de rajouter H2Tests, ça fait doublon car Gradle l'exécutera automatiquement puisque les tests sont marqués avec @Test
+    //PhonexTest.class, //ne pas l'ajouter, tests déjà marqués avec @Test
     StringTable1TestJ.class,
     StringTableTests.class,
     StringTransformersTest.class,
 })
+*/
 
 public class AllTestsSuite {
 
