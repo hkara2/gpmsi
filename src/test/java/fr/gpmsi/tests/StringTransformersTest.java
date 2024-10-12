6d2557f2018f2d3b7591df9da9edccde2ec3140a
Harry KARADIMAS <harry@karadimas.fr>
2024-10-12 23:38:31 +0200

    ajout commentaires javadoc

--- src/test/java/fr/gpmsi/tests/StringTransformersTest.java
+++ src/test/java/fr/gpmsi/tests/StringTransformersTest.java
@@ -7,8 +7,15 @@ import org.junit.Test;
 import fr.gpmsi.StringTransformable;
 import fr.gpmsi.StringTransformers;
 
+/**
+ * Test de {@link StringTransformers}
+ */
 public class StringTransformersTest {
 
+	/**
+	 * Tester la transformation de nombres français en nombres anglais. 1,3 -&gt; 1.3
+	 * @throws Exception _
+	 */
 	@Test
 	public void testFrenchToUsNumber1() throws Exception {
 		StringTransformable st = StringTransformers.frenchToUsNumber();
@@ -18,6 +25,10 @@ public class StringTransformersTest {
 		//System.out.println(y);
 	}
 
+	/**
+	 * Tester la transformation de nombres français en nombres anglais. 1,3E8 -&gt; 1.3E8
+	 * @throws Exception _
+	 */
 	@Test
 	public void testFrenchToUsNumber2() throws Exception {
 		StringTransformable st = StringTransformers.frenchToUsNumber();
@@ -27,6 +38,10 @@ public class StringTransformersTest {
 		assertEquals("1.3E8", y);
 	}
 
+	/**
+	 * Tester la transformation de nombres français en nombres anglais. -2E-8 -&gt; -2E-8
+	 * @throws Exception _
+	 */
 	@Test
 	public void testFrenchToUsNumber3() throws Exception {
 		StringTransformable st = StringTransformers.frenchToUsNumber();
@@ -36,8 +51,12 @@ public class StringTransformersTest {
 		assertEquals("-2E-8", y);
 	}
 
+	/**
+	 * Tester la transformation de dates au format français en dates au format iso
+	 * @throws Exception _
+	 */
 	@Test
-	public void testFrenchToUsDate() throws Exception {
+	public void testFrenchToIsoDate() throws Exception {
 		StringTransformable st = StringTransformers.frenchToIsoDate();
 		String x = "31/12/2016";
 		String y = st.transform(x);
