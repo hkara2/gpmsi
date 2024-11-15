e1ff1ec07ce1941eb7fc8ee2f191e500a0fb449c
hkaradimas <hkaradimas@ch-sudessonne.fr>
2024-11-15 22:18:02 +0100

    ajout options enc et sep

--- script-templates/groovy/csv_vers_xlsx_strings.groovy
+++ script-templates/groovy/csv_vers_xlsx_strings.groovy
@@ -6,17 +6,28 @@
  * -a:input chemin_du_fichier_csv_a_convertir
  * -a:output chemin_du_fichier_xlsx_en_sortie
  * -a:titre titre_de_la_feuille (si absent, le titre est "Feuil1")
+ * -a:sep code_hexa_separateur       : optionnel, code hexadécimal du séparateur. Par défaut 28 (point-virgule), sinon 2C (virgule)
+ * -a:enc encodage                   : optionnel, encodage du fichier. Par défaut windows-1252, sinon UTF-8 est courant aussi.
  *
  * Exemple d'exécution :
  * cd C:\Local\e-pmsi\fichiers-rss-mco\2021\M12\RSA
  * c:\app\gpmsi\v@PROJECT_VERSION@\gpmsi -script c:\app\gpmsi\v@PROJECT_VERSION@\scripts\groovy\csv_vers_xlsx_strings.groovy -a:input in\csv_vers_xlsx_donnees1.csv -a:output tmp-out\csv_vers_xlsx_donnees1.xlsx
  *
  * #240805 hk Création du fichier
+ * #241115 hk Ajout options sep et enc
  */
 
 import fr.gpmsi.CsvDestination
 import fr.gpmsi.poi.XlsxHelper
 import fr.gpmsi.StringTable
+
+sep = args.sep
+if (sep == null) '28' //par défaut séparateur ';'
+sepc = (Integer.parseInt(sep), 16) as char
+lg.debug "utilisation separateur '$sepc'"
+
+enc = args.enc
+if (enc == null) 'windows-1252' //par défaut encodage windows-1252
 
 title = args.titre
 if (title == null || title == "") title = "Feuil1"
@@ -28,6 +39,8 @@ csv {
 
     input args['input']
     output args['output']
+    csvSeparator sepc
+    inputEncoding enc
 
     onInit { //rien de plus a initialiser
     }
