0cd0c99d83cd3255d953fdb743f36daa6f4039af
Harry KARADIMAS <harry@karadimas.fr>
2024-12-04 00:37:16 +0100

    commentaires

--- src/main/groovy/fr/gpmsi/pmsi_rules/And.groovy
+++ src/main/groovy/fr/gpmsi/pmsi_rules/And.groovy
@@ -1,5 +1,9 @@
 package fr.gpmsi.pmsi_rules;
 
+/**
+ * Noeud "And" d'une AndOrExpr.
+ * C'est un noeud qui a 2 enfants.
+ */
 public class And extends AoeNode { 
   AoeNode left
   AoeNode right 
@@ -8,6 +12,10 @@ public class And extends AoeNode {
     left.dump(indent+"  ", sb)
     right.dump(indent+"  ", sb) 
     sb.append("$indent)\n")
+  }
+  @Override
+  public String toString() {
+      return "(and:"+String.valueOf(left)+","+String.valueOf(right)+")"
   }
 }
 
