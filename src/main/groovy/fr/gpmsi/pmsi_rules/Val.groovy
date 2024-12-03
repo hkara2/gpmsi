94025c3e8780a95f7699a10fa6d1d9f72c1016b5
Harry KARADIMAS <harry@karadimas.fr>
2024-12-04 00:37:51 +0100

    commentaires

--- src/main/groovy/fr/gpmsi/pmsi_rules/Val.groovy
+++ src/main/groovy/fr/gpmsi/pmsi_rules/Val.groovy
@@ -1,7 +1,16 @@
 package fr.gpmsi.pmsi_rules;
 
+/**
+ * Noeud "valeur" d'une AndOrExpr. La valeur est représentée par une String.
+ */
 public class Val extends AoeNode { 
   String val 
-  void dump(String indent, StringBuilder sb) { sb.append("$indent(v:$val)\n")}
+  void dump(String indent, StringBuilder sb) { sb.append("$indent(v:$val)\n") }
+  
+  @Override
+  public String toString() {
+      return ""+val
+  }
+  
 }
   
