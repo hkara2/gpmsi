c09ca3f6c2b15983e5770c69a4b12230182902fb
Harry KARADIMAS <harry@karadimas.fr>
2024-12-04 00:40:22 +0100

    ajout de criteres multiples

--- src/main/groovy/fr/gpmsi/pmsi_rules/PmsiCriterionRule.groovy
+++ src/main/groovy/fr/gpmsi/pmsi_rules/PmsiCriterionRule.groovy
@@ -2,18 +2,22 @@ package fr.gpmsi.pmsi_rules;
 
 import java.util.HashMap;
 
+import groovy.util.logging.Log4j2
+
 /**
- * Règle PMSI qui est juste construite sur un critère
+ * Classe de base de règle PMSI qui est juste construite sur un ou plusieurs critères PMSI,
+ * qui doivent tous être vérifiés pour que la règle soit déclenchée.
+ * Il faut implémenter "init" et "action".
  * @author hkaradimas
  */
-public class PmsiCriterionRule
+@Log4j2
+public abstract class PmsiCriterionRule
   implements PmsiRule 
 {
-
   PmsiCriterion criterion
   
-  public PmsiCriterionRule(PmsiCriterion criterion) {
-    this.criterion = criterion
+  public PmsiCriterionRule(PmsiCriterion... criteria) {
+    this.criterion = new PmsiAllCriteria(criteria)
   }
 
   @Override
@@ -21,10 +25,22 @@ public class PmsiCriterionRule
     return criterion.eval(context);
   }
 
+  /**
+   * Ici ne fait rien d'autre qu'envoyer un message
+   * @param context Le contexte
+   */
   @Override
-  public void init(HashMap context) { }
+  public void init(HashMap context) { 
+      log.info("Appel a init, context : "+context)
+  }
 
+  /**
+   * Ici ne fait rien d'autre qu'envoyer un message dans le log
+   * @param context Le contexte
+   */
   @Override
-  public void action(HashMap context) { }
+  public void action(HashMap context) { 
+      log.info("Appel a action, context : "+context)
+  }
 
 }
