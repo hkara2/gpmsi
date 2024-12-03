14c2a2c57af8e17a179e97c369522d5356b8ba1f
hkaradimas <hkaradimas@ch-sudessonne.fr>
2024-12-03 10:36:31 +0100

    prise en charge du deuxieme agument (taille)

--- script-templates/bat/LOCAL/start_menu.bat
+++ script-templates/bat/LOCAL/start_menu.bat
@@ -1,6 +1,8 @@
 rem Lancer le fichier html passe en parametre et dont les liens font
 rem un menu de fichiers et répertoire locaux
 rem N.B. on lance avec gpmsiw qui supprime la fenetre de terminal
+rem On peut ajouter la taille en deuxieme parametre si la taille automatique ne
+rem convient pas (ex : 800x600)
 
 setlocal EnableExtensions
 
@@ -8,5 +10,9 @@ if not defined GPMSI_HOME set GPMSI_HOME=C:\app\gpmsi\v@PROJECT_VERSION@
 
 set APP=%GPMSI_HOME%\gpmsiw.bat
 set SCRIPT=%GPMSI_HOME%\scripts\groovy\fr\gpmsi\local\start_menu.groovy
+if "_%2_"=="__" (
+    call %APP% -script %SCRIPT% -a:input %1
+) else (
+    call %APP% -script %SCRIPT% -a:input %1 -a:taille %2
+)
 
-call %APP% -script %SCRIPT% -a:input %1
