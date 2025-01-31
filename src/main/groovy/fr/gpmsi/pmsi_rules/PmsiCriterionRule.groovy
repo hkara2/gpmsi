2d0c2533b67de617fb2c6dd6ac9e995c99e099ca
hkaradimas <hkaradimas@ch-sudessonne.fr>
2025-01-31 18:36:41 +0100

    javadoc et clarification noms de variable

--- src/main/groovy/fr/gpmsi/pmsi_rules/PmsiCriterionRule.groovy
+++ src/main/groovy/fr/gpmsi/pmsi_rules/PmsiCriterionRule.groovy
@@ -7,40 +7,54 @@ import groovy.util.logging.Log4j2
 /**
  * Classe de base de règle PMSI qui est juste construite sur un ou plusieurs critères PMSI,
  * qui doivent tous être vérifiés pour que la règle soit déclenchée.
- * Il faut implémenter "init" et "action".
+ * Si on dérive cette classe, on peut étendre "init" et "action" pour des initialisations particulières
+ * ou une action à exécuter si eval renvoie "true".
  * @author hkaradimas
  */
 @Log4j2
-public abstract class PmsiCriterionRule
+public class PmsiCriterionRule
   implements PmsiRule 
 {
-  PmsiCriterion criterion
+  PmsiAllCriteria criteria
   
   public PmsiCriterionRule(PmsiCriterion... criteria) {
-    this.criterion = new PmsiAllCriteria(criteria)
-  }
-
-  @Override
-  public boolean eval(HashMap context) {
-    return criterion.eval(context);
+    this.criteria = new PmsiAllCriteria(criteria)
   }
 
   /**
-   * Ici ne fait rien d'autre qu'envoyer un message
+   * Ramène le résultat de l'évaluation des critères
+   * @return un boolean résultant de l'évaluation
+   */
+  @Override
+  public boolean eval(HashMap context) {
+    return criteria.eval(context);
+  }
+
+  /**
+   * Ici ne fait rien d'autre qu'envoyer un message de débogage dans le log.
+   * Etendre cette méthode dans une sous-classe pour faire des initialisations supplémentaires
    * @param context Le contexte
    */
   @Override
   public void init(HashMap context) { 
-      log.info("Appel a init, context : "+context)
+      log.debug("Appel a init, context : "+context)
   }
 
   /**
-   * Ici ne fait rien d'autre qu'envoyer un message dans le log
+   * Ici ne fait rien d'autre qu'envoyer un message de débogage dans le log.
+   * Etendre cette méthode dans une sous-classe pour faire des initialisations supplémentaires.
    * @param context Le contexte
    */
   @Override
   public void action(HashMap context) { 
-      log.info("Appel a action, context : "+context)
+      log.debug("Appel a action, context : "+context)
   }
 
+  /**
+   * Renvoie les critères de la règle
+   * @return les critères sous forme d'un objet PmsiAllCriteria
+   */
+  public PmsiAllCriteria getCriteria() {
+    return criteria;
+  }
 }
