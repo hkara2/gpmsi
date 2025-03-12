48d1d71d16858ff622ad2d04ed7bba51a9c45fe4
hkaradimas <hkaradimas@ch-sudessonne.fr>
2025-03-12 19:00:16 +0100

    creation d'une table avec infos de colonnes dans un csv

---
+++ script-templates/groovy/sql_create_table.groovy
@@ -0,0 +1,68 @@
+/**:encoding=UTF-8:
+ * A partir de définitions de colonnes, créer une instruction sql de création
+ * de table.
+ *
+ * Arguments :
+ * -a:table nom_de_la_table_a_creer
+ * -a:colmd chemin_du_fichier_de_definitions_de_colonnes
+ * -a:jdbcdriver nom_du_pilote_jdbc  (optionnel)
+ * -a:jdbcurl url_de_la_base         (optionnel)
+ * -a:jdbcuser user_de_la_base       (optionnel)
+ * -a:jdbcpwd mot_de_passe_du_user   (optionnel)
+ *
+ * Format des définitions de colonne :
+ * Fichier .csv, encodage ISO-8859-1
+ * Nom des colonnes :
+ * col_excel        Nom de la colonne dans le fichier excel
+ * col_sql          Nom de la colonne dans la base de donnees
+ * type_sql         Type SQL de la colonne
+ * type_jdbc        Type JDBC de la colonne (cf. java.sql.Types). Si vide, on utilise type_sql à la place
+ * longueur_sql     Longueur (pour VARCHAR, ou NUMERIC)
+ * echelle_sql      Echelle (correspond à SCALE pour NUMERIC ou DECIMAL)
+ * contrainte1_sql  Texte de contrainte a insérer avant la déclaration de type
+ * contrainte2_sql  Texte de contrainte a insérer après la déclaration de type
+ * format_donnee    Formatage de la donnée que l'on va retrouver dans le fichier (Date ou numérique, selon le type_sql). Optionnel.
+ *
+ * Exemple :
+ * c:\app\gpmsi\v2.0\gpmsi -script c:\app\gpmsi\v2.0\scripts\groovy\sql_create_table.groovy -a:table mouv -a:colmd etude_mouvements_I_PATIEN-v1_2_colonnes.csv
+ * c:\app\gpmsi\v2.0\gpmsi -script c:\app\gpmsi\v2.0\scripts\groovy\sql_create_table.groovy -a:table mouv -a:colmd etude_mouvements_I_PATIEN-v1_2_colonnes.csv -a:jdbcurl "jdbc:h2:./test" -a:jdbcuser sa -a:jdbcpwd "" -a:jdbcdriver "org.h2.Driver"
+ */
+
+import fr.gpmsi.StringTable
+import groovy.sql.Sql
+
+tableName = args.table
+colmd = new StringTable("COLMD", new File(args.colmd))
+
+sb = new StringBuffer()
+
+sb << "CREATE TABLE $tableName (\n"
+boolean first = true
+colmd.each {coldef ->
+    def colName = coldef.col_sql
+    def colType = coldef.type_sql
+    def len = coldef.longueur_sql ? coldef.longueur_sql as int : null
+    def scale = coldef.echelle_sql ? coldef.echelle_sql as int : null
+    def typeArgs = ""
+    if (scale != null) {
+        if (len == null) len = 0
+        typeArgs = "($len, $scale)"
+    }
+    else if (len != null) {
+        typeArgs = "($len)"
+    }
+    def constr1 = coldef.contrainte1_sql
+    def constr2 = coldef.contrainte1_sql
+    if (first) first = false
+    else sb << ",\n" //finir ligne précédente
+    sb << "  $colName $constr1 $colType$typeArgs $constr2 /* $coldef.col_excel */"
+}
+sb << "\n)"
+println sb
+
+//maintenant si le jdbc url existe, se connecter à la base et créer la table
+if (args.jdbcurl != null) {
+    println "Creation de la table $tableName ..."
+    def sqlConn = Sql.newInstance(args.jdbcurl, args.jdbcuser, args.jdbcpwd, args.jdbcdriver)
+    sqlConn.execute sb.toString()
+}
