8efe74a0b59d0c247305fb141ea1a8236187f034
hkaradimas <hkaradimas@ch-sudessonne.fr>
2026-02-08 15:09:55 +0100

    Adaptations aux usages plus actuels

--- script-templates/groovy/gabarit_script.groovy
+++ script-templates/groovy/gabarit_script.groovy
@@ -1,10 +1,13 @@
-//☺:encoding=UTF-8:
-
-/**
- * Gabarit pour un nouveau script. A modifier selon les besoins
+/**☺:encoding=UTF-8:
+ * Gabarit pour un nouveau script. A modifier selon les besoins.
+ * Contient des exemples pour l'utilisation de input, output, un argument, un flag.
  * Arguments :
  * -a:input chemin_du_fichier_entree
  * -a:output chemin_du_fichier_sortie
+ * -a:monarg un_argument_exemple
+ * Drapeaux :
+ * -f:myflag lever le drapeau myflag
+ *
  * Exemples :
  * C:\app\gpmsi\v@PROJECT_VERSION@\gpmsi -script C:\app\gpmsi\v@PROJECT_VERSION@\scripts\groovy\gabarit_script.groovy -a:input test.txt -a:output testout.txt
  */
@@ -16,6 +19,11 @@ line {
     onInit {
         //ouvrir la destination
         outf = new FileWriter(outputFilePath)
+        //donner la valeur de l'argument monarg
+        monarg = args['monarg']
+        println "monarg : $monarg"
+        //indiquer que le drapeau a été levé
+        if (flags.contains('myflag')) println "option myflag detectee"
     }
     onItem {item->
         //ecrire numéro de ligne, suivi du contenu de la ligne
