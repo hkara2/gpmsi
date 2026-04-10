57e978970100a1f822dbaa844b4140d4ce612474
hkaradimas <hkaradimas@ch-sudessonne.fr>
2026-04-10 19:28:04 +0200

    ajout constructeur avec closure

--- src/main/groovy/fr/gpmsi/pmsi_rules/rsa/RsaRule.groovy
+++ src/main/groovy/fr/gpmsi/pmsi_rules/rsa/RsaRule.groovy
@@ -1,14 +1,24 @@
 package fr.gpmsi.pmsi_rules.rsa
-import fr.gpmsi.pmsi_rules.PmsiRule
+import groovy.lang.Closure;
 import fr.gpmsi.pmsixml.FszGroup
+import fr.gpmsi.pmsi_rules.PmsiRule
+import fr.gpmsi.pmsi_rules.GenericPmsiCriterion
+import fr.gpmsi.pmsi_rules.PmsiCriterion
 
 /**
  * Règle qui est spécialisée pour les RSAs.
  * Etendre un objet et implementer init/eval/action
+ * Ou sinon utiliser le constructeur qui prend une Closure en argument
  */
 class RsaRule
     implements PmsiRule 
 {
+  PmsiCriterion crit
+  
+  public RsaRule() {}
+  
+  public RsaRule(Closure c) { crit = new GenericPmsiCriterion(c) }
+  
   void init(HashMap context) {
   }
   
@@ -16,7 +26,10 @@ class RsaRule
       eval(context, context['rsa'])
   }
   
-  boolean eval(HashMap context, FszGroup rsa) {}
+  boolean eval(HashMap context, FszGroup rsa) {
+      if (crit) return crit.eval(context)
+      else return false
+  }
 
   void action(HashMap context) {
       action(context, context['rsa'])
