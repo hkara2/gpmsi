15ea3a0c760e8b2e5d340dcdc3d293fcc99586e9
hkaradimas <hkaradimas@ch-sudessonne.fr>
2026-06-21 20:06:21 +0200

    nouveaux tests pour les nouveautés dao

--- src/test/groovy/fr/gpmsi/da/tests/DaoTest.groovy
+++ src/test/groovy/fr/gpmsi/da/tests/DaoTest.groovy
@@ -25,52 +25,52 @@ class DaoTest {
 
   @Test
   public void testDdlGen1() {
-    Dao d1 = new Dao('address')
-    d1.pkcol(new CInteger('address_id'))
-    d1.col(new CVarchar('city', 64))
+    Dao d1 = new Dao('ADDRESS')
+    d1.pkcol(new CInteger('ADDRESS_ID'))
+    d1.col(new CVarchar('CITY', 64))
     def str = d1.makeTableDdl(nl+'/* extra SQL */', 'H2')
     //println str
-    assertEquals("CREATE TABLE address (${nl}address_id BIGINT PRIMARY KEY,${nl}city VARCHAR(64)$nl/* extra SQL */$nl)" as String, str)
+    assertEquals("CREATE TABLE IF NOT EXISTS ADDRESS (${nl}ADDRESS_ID BIGINT PRIMARY KEY,${nl}CITY VARCHAR(64)$nl/* extra SQL */$nl)" as String, str)
   }
 
   @Test
   public void testDdlGen2() {
-    Dao d1 = new Dao('person')
-    d1.pkcol(new CInteger(  'person_id'     ))
-    d1.pkcol(new CVarchar(  'last_name',  64))
-    d1.col(new CVarchar(    'first_name', 64))
-    d1.col(new CDate(       'birthdate'     ))
+    Dao d1 = new Dao('PERSON')
+    d1.pkcol(new CInteger(  'PERSON_ID'     ))
+    d1.pkcol(new CVarchar(  'LAST_NAME',  64))
+    d1.col(new CVarchar(    'FIRST_NAME', 64))
+    d1.col(new CDate(       'BIRTHDATE'     ))
     def str = d1.makeTableDdl(null, 'H2')
-    println str
+    //println str
     //assertEquals("CREATE TABLE address (${nl}address_id BIGINT PRIMARY KEY,${nl}city VARCHAR(64)$nl/* extra SQL */$nl)" as String, str)
   }
 
   @Test
   public void testDdlGen3() {
-    Dao d1 = new Dao('person')
-    d1.pkcol(new CInteger(  'person_id', true))
-    d1.pkcol(new CVarchar(  'last_name',   64))
-    d1.col(new CVarchar(    'first_name',  64))
-    d1.col(new CDate(       'birthdate'      ))
+    Dao d1 = new Dao('PERSON')
+    d1.pkcol(new CInteger(  'PERSON_ID', true))
+    d1.pkcol(new CVarchar(  'LAST_NAME',   64))
+    d1.col(new CVarchar(    'FIRST_NAME',  64))
+    d1.col(new CDate(       'BIRTHDATE'      ))
     def str = d1.makeTableDdl(null, 'H2')
-    println str
+    //println str
     //assertEquals("CREATE TABLE address (${nl}address_id BIGINT PRIMARY KEY,${nl}city VARCHAR(64)$nl/* extra SQL */$nl)" as String, str)
   }
 
   @Test
   public void testDdlGen4() {
-    Dao d1 = new Dao('person')
-    d1.pkcol(new CInteger(  'person_id', true))
-    d1.col(new CVarchar(    'last_name',   64))
-    d1.col(new CVarchar(    'first_name',  64))
-    d1.col(new CDate(       'birthdate'      ))
+    Dao d1 = new Dao('PERSON')
+    d1.pkcol(new CInteger(  'PERSON_ID', true))
+    d1.col(new CVarchar(    'LAST_NAME',   64))
+    d1.col(new CVarchar(    'FIRST_NAME',  64))
+    d1.col(new CDate(       'BIRTHDATE'      ))
     def str = d1.makeTableDdl(null, 'H2')
     //println str
-    def expected = """CREATE TABLE person (
-person_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-last_name VARCHAR(64),
-first_name VARCHAR(64),
-birthdate DATE
+    def expected = """CREATE TABLE IF NOT EXISTS PERSON (
+PERSON_ID BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+LAST_NAME VARCHAR(64),
+FIRST_NAME VARCHAR(64),
+BIRTHDATE DATE
 )"""
     //println expected
     //assertEquals(expected, str) //ceci ne fonctionne pas à cause des fins de ligne
@@ -79,18 +79,18 @@ birthdate DATE
 
   @Test
   public void testDdlGen5() {
-    Dao d1 = new Dao('person')
-    d1.col(new CInteger(  'person_id'      ))
-    d1.col(new CVarchar(    'last_name',   64))
-    d1.col(new CVarchar(    'first_name',  64))
-    d1.col(new CDate(       'birthdate'      ))
+    Dao d1 = new Dao('PERSON')
+    d1.col(new CInteger(  'PERSON_ID'      ))
+    d1.col(new CVarchar(    'LAST_NAME',   64))
+    d1.col(new CVarchar(    'FIRST_NAME',  64))
+    d1.col(new CDate(       'BIRTHDATE'      ))
     def str = d1.makeTableDdl(null, 'H2')
-    println str
-    def expected = """CREATE TABLE person (
-person_id BIGINT,
-last_name VARCHAR(64),
-first_name VARCHAR(64),
-birthdate DATE
+    //println str
+    def expected = """CREATE TABLE IF NOT EXISTS PERSON (
+PERSON_ID BIGINT,
+LAST_NAME VARCHAR(64),
+FIRST_NAME VARCHAR(64),
+BIRTHDATE DATE
 )"""
     //println expected
     //assertEquals(expected, str) //ceci ne fonctionne pas à cause des fins de ligne
