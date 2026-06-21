533812045e1dec1922856a50c1b26de30b943e64
hkaradimas <hkaradimas@ch-sudessonne.fr>
2026-06-21 14:02:37 +0200

    méthodes de test pour classe Chrono

---
+++ src/test/java/fr/gpmsi/tests/ChronoTests.java
@@ -0,0 +1,32 @@
+package fr.gpmsi.tests;
+
+import static org.junit.Assert.*;
+
+import java.time.temporal.ChronoUnit;
+
+import org.junit.Test;
+
+import fr.gpmsi.Chrono;
+
+public class ChronoTests {
+  private static boolean emitDebugMessages = false;
+
+  @Test
+  public void testChrono1()
+      throws Exception
+  {
+    Chrono c1 = new Chrono();
+    c1.setMark(c1.getMark() - 10_000); //reculer de 10 secondes
+    if (emitDebugMessages) System.out.println("c1:" + c1.messageEn("Temps passé : ", ChronoUnit.SECONDS) + c1.messageEn(", soit ", ChronoUnit.MILLIS, "."));
+  }
+  
+  @Test
+  public void testChrono2()
+      throws Exception
+  {
+    Chrono c2 = new Chrono();
+    c2.setMark(c2.getMark() - 1000); //reculer de 1 seconde
+    if (emitDebugMessages) System.out.println("c2:" + c2.messageFr("Temps passé : ", ChronoUnit.SECONDS) + c2.messageFr(", soit ", ChronoUnit.MILLIS, "."));
+  }
+  
+}
