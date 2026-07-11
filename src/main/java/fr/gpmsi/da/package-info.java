5d1f1f8d85fca7adc8f94f3a24fb3f7b25471b2e
hkaradimas <hkaradimas@ch-sudessonne.fr>
2026-07-11 16:00:08 +0200

    changement des commentaires

--- src/main/java/fr/gpmsi/da/package-info.java
+++ src/main/java/fr/gpmsi/da/package-info.java
@@ -1,6 +1,7 @@
 /**
  * @author hkaradimas
- * Data Access Object (DAO) : classes pour faire un objet d'accès aux données, qui permet de faciliter l'accès à une table de base de données ou bien à des rangées d'un fichier .csv
- * Réservé pour un usage futur. 
+ * Data Access : classes pour faciliter l'accès aux données (Data Access), notamment en construisant des classes
+ * pour des objets d'accès aux données (DAO ou Data Access Object), ce qui permet de faciliter l'accès à une
+ * table de base de données ou bien à des échanges avec des rangées d'un fichier .csv
  */
 package fr.gpmsi.da;
