91f051ecdad10c9ab3e4ea7eb354f9762dc0b8b3
hkaradimas <hkaradimas@ch-sudessonne.fr>
2026-07-11 16:00:47 +0200

    correction bug qui n'injectait pas avec le parametre injrssId

--- src/main/groovy/fr/gpmsi/da/rss/RssInjector.groovy
+++ src/main/groovy/fr/gpmsi/da/rss/RssInjector.groovy
@@ -16,21 +16,47 @@ class RssInjector {
   
   Sql connection
   
+  /**
+   * Constructeur sans arguments
+   */
+  RssInjector() {}
+  
+  /**
+   * Constructeur avec la connection à utiliser
+   * @param connection La connection sql
+   */
+  RssInjector(Sql connection) {
+    this.connection = connection
+  }
+  
+  /**
+   * A partir du Reader fourni, lire les Rums et les injecter en table
+   * @param rdr Le Reader
+   * @param injrssId L'id d'injection
+   */
   void injectRss(Reader rdr, Long injrssId) {
     Rum rum = new Rum()
     RssReader rssrdr = new RssReader();
     rdr.eachLine {line->
       FszNode rumNd = rssrdr.readOne(line)
-      rum.insertRum(connection, rumNd)
+      rum.insertRum(connection, rumNd, injrssId)
     }
     //créer une table RSS qui permet de raisonner au niveau du RSS
     Rss.instance.fillFromRum(connection)
   }
 
+  /**
+   * Ramener la connection
+   * @return la connection
+   */
   public Sql getConnection() {
     return connection;
   }
 
+  /**
+   * Définir la connection
+   * @param connection la connection
+   */
   public void setConnection(Sql connection) {
     this.connection = connection;
   }
