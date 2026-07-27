4e8563ed16f7ef2c42c3de2d850ff75ece9d521f
hkara2 <harry@karadimas.fr>
2026-07-27 22:17:52 +0200

    commentaires + testGetUtf8StringResource

--- src/test/java/fr/gpmsi/tests/StringUtilsTests.java
+++ src/test/java/fr/gpmsi/tests/StringUtilsTests.java
@@ -6,8 +6,14 @@ import org.junit.Test;
 
 import fr.gpmsi.StringUtils;
 
+/**
+ * Tests pour StringUtils
+ */
 public class StringUtilsTests {
 
+  /**
+   * Test normalisation de ligne 1
+   */
   @Test
   public void testNormalizeNewlines1() {
     String a = "foo\nbar\nbaz";
@@ -15,6 +21,9 @@ public class StringUtilsTests {
     assertEquals(b, "foo@bar@baz");
   }
 
+  /**
+   * Test normalisation de ligne 2
+   */
   @Test
   public void testNormalizeNewlines2() {
     String a = "foo\rbar\rbaz";
@@ -22,6 +31,9 @@ public class StringUtilsTests {
     assertEquals(b, "foo@bar@baz");
   }
 
+  /**
+   * Test normalisation de ligne 3
+   */
   @Test
   public void testNormalizeNewlines3() {
     String a = "foo\r\nbar\r\nbaz";
@@ -29,6 +41,9 @@ public class StringUtilsTests {
     assertEquals(b, "foo@bar@baz");
   }
 
+  /**
+   * Test normalisation de ligne 4
+   */
   @Test
   public void testNormalizeNewlines4() {
     String a = "foo\r\nbar\r\nbaz\r";
@@ -36,6 +51,9 @@ public class StringUtilsTests {
     assertEquals(b, "foo@bar@baz@");
   }
 
+  /**
+   * Test normalisation de ligne 5
+   */
   @Test
   public void testNormalizeNewlines5() {
     String a = "foo\r\nbar\r\r\nbaz\r";
@@ -43,4 +61,17 @@ public class StringUtilsTests {
     assertEquals(b, "foo@bar@@baz@");
   }
 
+  /**
+   * Test de la méthode getUtf8StringResource
+   * @throws Exception Ne devrait jamais se produire
+   */
+  @Test
+  public void testGetUtf8StringResource()
+      throws Exception
+  {
+      String str = StringUtils.getUtf8StringResource("/fr/gpmsi/pmsixml/vidhospV016.csv");
+      assertNotNull("\"/fr/gpmsi/pmsixml/vidhospV016.csv\" devrait être trouvé", str);
+      //System.out.println("\"/fr/gpmsi/pmsixml/vidhospV016.csv\" : "+str);
+      assertTrue("La resource devrait commencer par :Typ;Libellé;Nomc", str.startsWith("Typ;Libellé;Nomc"));
+  }
 }
